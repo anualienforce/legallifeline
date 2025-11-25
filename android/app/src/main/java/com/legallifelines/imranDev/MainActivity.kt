d88872114df69346aa6fc2b18f414d48d5a857e9
anandprajapati <anusp979@gmail.com>
2025-11-25 09:35:55 +0530

    first commit

--- android/app/src/main/java/com/legallifelines/imranDev/MainActivity.kt
+++ android/app/src/main/java/com/legallifelines/imranDev/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.alienorce.legallifelines
+package com.legallifelines.imranDev
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/legallifelines/imranDev/MainApplication.kt
+++ android/app/src/main/java/com/legallifelines/imranDev/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.alienorce.legallifelines
+package com.legallifelines.imranDev
 
 import android.app.Application
 import android.content.res.Configuration
